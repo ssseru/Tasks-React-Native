3dd212f19f60ba11c410aab29cfacfcc0fb32bc5
Sai Sagar Seru <saisseru@gmail.com>
2020-08-29 16:43:43 +0530

    Added InputText, Button & Flatlist

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [currentTask, setCurrentTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  const buttonOnPress = () => {
+    setTaskList((taskList) => [
+      ...taskList,
+      { id: Math.random().toString(), value: currentTask },
+    ]);
+  };
+
+  const taskInputHandler = (enteredText) => {
+    setCurrentTask(enteredText);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.root}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Task"
+          onChangeText={taskInputHandler}
+          value={currentTask}
+        />
+        <Button title="ADD" onPress={buttonOnPress} />
+      </View>
+      <FlatList
+        data={taskList}
+        renderItem={(itemData) => (
+          <View>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  root: {
+    padding: 40,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    width: "80%",
+  },
+  listItems: {
+    flexDirection: "column",
+    alignItems: "center",
   },
 });
